a1b31bbded8099d20627d0b5f352c2cb36b8f88c
John Delano <jdelano@cedarville.edu>
2021-03-19 12:11:23 -0400

    Ready for Service Layer Part 2 Start

---
+++ TMS.Service/Controllers/BaseController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TMS.Business.Core;
+using TMS.Common.Core;
+
+namespace TMS.Service.Controllers
+{
+    public class BaseController : Controller
+    {
+        private IActionManager manager;
+        private ILogger logger;
+
+        public BaseController(IActionManager manager, ILogger logger)
+        {
+            this.manager = manager;
+            this.logger = logger;
+        }
+
+        public IActionManager ActionManager
+        {
+            get
+            {
+                return manager;
+            }
+        }
+
+        public ILogger Logger
+        {
+            get
+            {
+                return logger;
+            }
+        }
+
+        public void LogException(Exception ex)
+        {
+            string errorMessage = LoggerHelper.GetExceptionDetails(ex);
+            logger.LogError(LoggingEvents.SERVICE_ERROR, ex, errorMessage);
+        }
+    }
+}
---
+++ TMS.Service/Filters/LoggingActionFilter.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TMS.Common.Core;
+using TMS.Service.Controllers;
+
+namespace TMS.Service.Filters
+{
+    public class LoggingActionFilter : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            Log("OnActionExecuting", context.RouteData, context.Controller);
+        }
+
+        public override void OnActionExecuted(ActionExecutedContext context)
+        {
+            Log("OnActionExecuted", context.RouteData, context.Controller);
+
+        }
+
+        public override void OnResultExecuted(ResultExecutedContext context)
+        {
+            Log("OnResultExecuted", context.RouteData, context.Controller);
+        }
+
+        public override void OnResultExecuting(ResultExecutingContext context)
+        {
+            Log("OnResultExecuting", context.RouteData, context.Controller);
+        }
+
+        private void Log(string methodName, RouteData routeData, object controller)
+        {
+            var controllerName = routeData.Values["controller"];
+            var actionName = routeData.Values["action"];
+            var message = string.Format($"{methodName} controller: {controllerName} action: {actionName}");
+            BaseController baseController = (BaseController)controller;
+            baseController.Logger.LogInformation(LoggingEvents.ACCESS_METHOD, message);
+        }
+    }
+}
